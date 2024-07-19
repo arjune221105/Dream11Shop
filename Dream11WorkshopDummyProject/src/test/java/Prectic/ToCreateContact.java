e4a08c6540aaf593faef248fd1c098c37ad51bbb
arjune221105 <shakti221105@gmail.com>
2024-07-19 01:25:14 -0700

    Showing Messing Dream``

---
+++ Dream11WorkshopDummyProject/src/test/java/Prectic/ToCreateContact.java
@@ -0,0 +1,10 @@
+package Prectic;
+
+public class ToCreateContact {
+
+	public static void main(String[] args) {
+		System.out.println("Dream 11 Dummy Project");
+		System.out.println("Dream 12 Dummy Project Showin on Display");
+	}
+
+}
